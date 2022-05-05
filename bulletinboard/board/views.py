c1b7a033d698b18c6ff804c0f88bc50ff1fd852c
Krole <kr.oleg@gmail.com>
2022-05-05 21:27:28 +0300

    Create app board

--- bulletinboard/board/views.py
+++ bulletinboard/board/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from board.models import Bb
+
+
+def index(request):
+    title = 'Список объявлений\r\n\r\n\r\n'
+
+    for x in Bb.objects.order_by('-published'):
+        title += f'{x.title}\r\n{x.content}\r\n\r\n'
+
+    return HttpResponse(title, content_type='text/plain; charset=utf-8')
